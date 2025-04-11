33cfc3591833ceff872bd905553d84ee18e05f41
Aman <avman1998@gmail.com>
2025-04-11 22:54:03 +0530

    comment

--- src/usePrevious.js
+++ src/usePrevious.js
@@ -1,9 +1,11 @@
 import { useRef, useEffect } from "react";
 function usePrevious(value) {
   const ref = useRef();
+  // This will run second
   useEffect(() => {
     ref.current = value;
   }, [value]);
+  //This is run first
   return ref.current;
 }
 export default usePrevious;
